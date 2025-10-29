fd1e881ee3d70dd72c64714061d982abc2fd981b
385944 <a385944@uach.mx>
2025-10-29 08:00:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 st.title("ejmplo para usar session_state")
 
+if "key" not in st.session_state:
+  st.session_state["key"] = "value"
+
 count = 0
 
 increment = st.button("increment")
@@ -8,3 +11,5 @@ if increment:
   count += 1
 
 st.write("count = ", count)
+
+st.write(st.session_state)
